432559e5b43fa4cf89d2f7d8d709577a666289ab
Ponsiano De Loor <ponsianodeloor@gmail.com>
2023-03-02 00:15:45 -0500

    parentesis balanceados

---
+++ 001_desafio_de_texto/006_parentesis_balanceados.py
@@ -0,0 +1,17 @@
+def parentesis_balanceados(texto):
+
+    apertura = 0
+
+    for parentesis in texto:
+        if parentesis == "(":
+            apertura += 1
+        elif parentesis == ")":
+            apertura -= 1
+            if apertura < 0:
+                return False
+    return
+
+
+print(parentesis_balanceados("((()))()"))
+print(parentesis_balanceados(")(()"))
+print(parentesis_balanceados("(()"))
